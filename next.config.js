8c743800f2a0af54e4eef76cf2eba0d9dae69414
PaanJoe <farhan.gunz@gmail.com>
2023-02-07 01:00:24 +0800

    config remove

--- next.config.js
+++ next.config.js
@@ -2,19 +2,10 @@
 /** @type {import('next-sitemap').IConfig} */
 
 module.exports = {
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://ustazgpt.paanjoe.com/api/:path*',
-      },
-    ]
-  }, 
   reactStrictMode: false,
   env: {
     GOOGLE_ANALYTICS: process.env.GOOGLE_ANALYTICS,
     siteUrl: process.env.SITE_URL,
-    generateRobotsTxt: true,
-    NEXTAUTH_URL: process.env.NEXTAUTH_URL
+    generateRobotsTxt: true
   }
 };
